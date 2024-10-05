aa1907fbe836c244f6430d7796375f1b1dfcc55d
bryannoels <bryannoel.salindeho@gmail.com>
2024-10-06 02:30:07 +0800

    feat: add layout to base route

--- Laba/src/navigation/router.tsx
+++ Laba/src/navigation/router.tsx
@@ -3,18 +3,15 @@ import {
     createRoutesFromElements,
     Route
   } from "react-router-dom";
-import App from '../App.tsx'
+import Layout from '../Layout';
 import Dashboard from "../pages/dashboard/Dashboard.tsx";
 import Login from "../pages/login/Login.tsx"
-import Navbar from "../pages/navbar/Navbar.tsx"
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
-        <>
-            <Route path="/" element={<App />} />
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="login" element={<Login />} />
-            <Route path="navbar" element={<Navbar />} />
-        </>
+    <Route path="/" element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="login" element={<Login />} />
+      </Route>
     )
 );
