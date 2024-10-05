26b13e0fa74d098ac155b41d5d4f596d48fa73b5
bryannoels <bryannoel.salindeho@gmail.com>
2024-10-06 02:30:30 +0800

    feat: add dummy dashboard

--- Laba/src/pages/dashboard/Dashboard.tsx
+++ Laba/src/pages/dashboard/Dashboard.tsx
@@ -3,10 +3,10 @@ import './Dashboard.css'
 
 function Dashboard() {
   return (
-    <>
+    <div className = "dashboard">
       <img src={labaLogo} className="logo" alt="Laba logo" />
       <h1>Dashboard Page</h1>
-    </>
+    </div>
   )
 }
 
