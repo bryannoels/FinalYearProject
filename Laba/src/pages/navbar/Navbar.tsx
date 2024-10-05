23e9dbd914a47f6e237a0f99a32755439a0c020a
bryannoels <bryannoel.salindeho@gmail.com>
2024-10-06 02:29:55 +0800

    feat: navbar page

--- Laba/src/pages/navbar/Navbar.tsx
+++ Laba/src/pages/navbar/Navbar.tsx
@@ -1,13 +1,24 @@
 import labaLogo from '../../assets/LabaLogo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 import './Navbar.css'
 
 function Navbar() {
   return (
-    <>
-      <img src={labaLogo} className="logo" alt="Laba logo" />
-      <h1>Navbar Page</h1>
-    </>
+      <nav className="navbar">
+        <Link to="/" className="navbar__logo">
+          <img src={labaLogo} alt="labaLogo" className = "navbar__icon" />
+          <p className = "navbar__company_name">LABA</p>
+        </Link>
+        <div className = "navbar__menu">
+          <FontAwesomeIcon icon={faSearch} aria-hidden="true" className="navbar__search"/>
+          <Link to="/login">
+            <button className="navbar__button">Log in</button>
+          </Link>
+        </div>
+      </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
