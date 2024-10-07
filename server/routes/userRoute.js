6c9a8f909bac6e654a2e1dde08febbe65b5476bc
bryannoels <bryannoel.salindeho@gmail.com>
2024-10-07 22:47:04 +0800

    feat: create GET endpoint

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -2,10 +2,18 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/userModel')
 
-router.get('/', () => {})
-router.post('/', async (req,res) => {
-    const {username, email, password} = req.body
-    try{
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find()
+        res.status(200).json(users)
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+})
+
+router.post('/', async (req, res) => {
+    const { username, email, password } = req.body
+    try {
         const user = await User.create({
             username,
             email,
@@ -13,9 +21,9 @@ router.post('/', async (req,res) => {
             portfolio: []
         })
         res.status(201).json(user)
-    }
-    catch(err){
-        res.status(400).json(err)
+    } catch (err) {
+        res.status(400).json({ error: err.message })
     }
 })
+
 module.exports = router
