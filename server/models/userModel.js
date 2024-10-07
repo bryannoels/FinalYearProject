43a1a7d660781022098073133077d05ebd238a21
bryannoels <bryannoel.salindeho@gmail.com>
2024-10-07 22:45:05 +0800

    feat: create user model

---
+++ server/models/userModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    userId: { type: Schema.Types.ObjectId, auto: true },
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    portfolio: { type: [Number], default: [] }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
