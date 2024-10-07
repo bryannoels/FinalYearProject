b0643a63a6167b3dd3784292749564450ecdbf3a
bryannoels <bryannoel.salindeho@gmail.com>
2024-10-07 22:45:16 +0800

    feat: update server.js

--- server/server.js
+++ server/server.js
@@ -1,18 +1,19 @@
 require('dotenv').config()
 
 const express = require('express')
-const usersRoutes = require('./routes/users')
+const userRoutes = require('./routes/userRoute')
 
 const mongoose = require('mongoose')
 const app = express()
 
+app.use(express.json())
 
 app.use((req,res,next) => {
     console.log(req.path, req.method)
     next()
 })
 
-app.use('api/users/',usersRoutes)
+app.use('/api/users/',userRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
