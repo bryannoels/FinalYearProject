f2d7692851bbea8b8df7b5a60dcd4b611cdf12ad
bryannoels <bryannoel.salindeho@gmail.com>
2024-10-08 20:11:35 +0800

    build: add cors to express

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const cors = require('cors');
 const userRoutes = require('./routes/userRoute')
 const stockRoutes = require('./routes/stockRoute')
 
@@ -8,6 +9,7 @@ const mongoose = require('mongoose')
 const app = express()
 
 app.use(express.json())
+app.use(cors());
 
 app.use((req,res,next) => {
     console.log(req.path, req.method)
