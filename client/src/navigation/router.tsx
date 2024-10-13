6724c9c92d94abdc5f3cda015dd2b3c3db26faa0
bryannoels <bryannoel.salindeho@gmail.com>
2024-10-14 01:09:31 +0800

    feat: add routing of stock detail

--- client/src/navigation/router.tsx
+++ client/src/navigation/router.tsx
@@ -6,12 +6,14 @@ import {
 import Layout from '../Layout.tsx';
 import Dashboard from "../pages/dashboard/Dashboard.tsx";
 import Login from "../pages/login/Login.tsx"
+import StockDetails from "../pages/stockDetails/stockDetails.tsx";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Layout />}>
         <Route index element={<Dashboard />} />
         <Route path="login" element={<Login />} />
+        <Route path="stock/:symbol" element={<StockDetails />} />
       </Route>
     )
 );
