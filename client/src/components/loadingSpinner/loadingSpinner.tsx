1993345fd07b8bb01156babd6ab0cfd7b3a0f530
bryannoels <bryannoel.salindeho@gmail.com>
2024-11-13 03:11:33 +0800

    feat: create loading spinner

---
+++ client/src/components/loadingSpinner/loadingSpinner.tsx
@@ -0,0 +1,11 @@
+import './loadingSpinner.css';
+
+const LoadingSpinner = () => {
+    return (
+        <div className="loading-spinner">
+            <div className="spinner"></div>
+        </div>
+    );
+};
+
+export default LoadingSpinner;
