ef4fe59db7d6070a2fa340f01768edfd110ef572
bryannoels <bryannoelsalindeho23@gmail.com>
2025-01-04 00:34:36 +0800

    feat: create stock profile type

---
+++ client/src/types/StockProfile.tsx
@@ -0,0 +1,14 @@
+export type StockProfile = {
+    companyName: string;
+    sector: string;
+    industry: string;
+    country: string;
+    state: string;
+    city: string;
+    address: string;
+    phone: string;
+    website: string;
+    CEO: string;
+    fullTimeEmployees: string;
+    longBusinessSummary: string;
+};
