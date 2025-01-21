b92a7f5e80925d2e42cb5de99bd899f4d9949426
bryannoels <bryannoelsalindeho23@gmail.com>
2025-01-22 01:51:42 +0800

    feat: add stock forecase

---
+++ client/src/components/stockDetails/stockForecast.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Stock } from '../../types/Stock';
+
+interface StockForecastProps {
+  stockData: Stock | null;
+}
+
+const StockForecast: React.FC<StockForecastProps> = ({ stockData }) => {
+    if (stockData == null || stockData.forecast == null) return null;
+
+    return (
+        <div className="stock-details__forecast">
+            <div className="stock-details__forecast__row">
+                <div className="stock-details__forecast__label">High Target Price</div>
+                <div className="stock-details__forecast__right green-rating">
+                    <div className="stock-details__forecast__value">{stockData.forecast.high_target_price}</div>
+                    <div className="stock-details__forecast__percent">({stockData.forecast.percent_high_price.toFixed(1)}%)</div>
+                </div>
+            </div>
+            <div className="stock-details__forecast__row">
+                <div className="stock-details__forecast__label">Median Target Price</div>
+                <div className="stock-details__forecast__right blue-rating">
+                    <div className="stock-details__forecast__value">{stockData.forecast.median_target_price}</div>
+                    <div className="stock-details__forecast__percent">({stockData.forecast.percent_median_price.toFixed(1)}%)</div>
+                </div>
+            </div>
+            <div className="stock-details__forecast__row">
+                <div className="stock-details__forecast__label">Low Target Price</div>
+                <div className="stock-details__forecast__right red-rating">
+                    <div className="stock-details__forecast__value">{stockData.forecast.low_target_price}</div>
+                    <div className="stock-details__forecast__percent">({stockData.forecast.percent_low_price.toFixed(1)}%)</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default StockForecast;
