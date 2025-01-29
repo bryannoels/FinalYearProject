840c7f876b03f66b59c42b304891f27d37c03d78
bryannoels <bryannoelsalindeho23@gmail.com>
2025-01-30 04:38:50 +0800

    feat: update the fields on stock verdict

--- client/src/components/stockDetails/stockVerdict.tsx
+++ client/src/components/stockDetails/stockVerdict.tsx
@@ -11,19 +11,19 @@ const StockVerdict: React.FC<StockVerdictProps> = ({ stockData }) => {
     return (
         <div className="stock-details__analysis">
             <div className="stock-detailss__analysis__left">
-                <p className={`stock-detailss__analysis__left__text ${stockData.analysis.number_of_buys}`}>{stockData.analysis.number_of_buys}</p>
+                <p className={`stock-detailss__analysis__left__text ${stockData.analysis.verdict}`}>{stockData.analysis.verdict}</p>
             </div>
             <div className="stock-detailss__analysis__right">
                 <div className="stock-detailss__analysis__buys">
-                    <div className="stock-detailss__analysis__value">{stockData.analysis.number_of_buys}</div>
+                    <div className="stock-detailss__analysis__value">{stockData.analysis.number_of_buy}</div>
                     <div className="stock-detailss__analysis__text">buys</div>
                 </div>
                 <div className="stock-detailss__analysis__holds">
-                    <div className="stock-detailss__analysis__value">{stockData.analysis.number_of_holds}</div>
+                    <div className="stock-detailss__analysis__value">{stockData.analysis.number_of_hold}</div>
                     <div className="stock-detailss__analysis__text">holds</div>
                 </div>
                 <div className="stock-detailss__analysis__sells">
-                    <div className="stock-detailss__analysis__value">{stockData.analysis.number_of_sells}</div>
+                    <div className="stock-detailss__analysis__value">{stockData.analysis.number_of_sell}</div>
                     <div className="stock-detailss__analysis__text">sells</div>
                 </div>
             </div>
