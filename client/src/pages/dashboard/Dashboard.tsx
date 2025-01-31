661cb369408f6f5f21c284598f2d084232178d07
bryannoels <bryannoelsalindeho23@gmail.com>
2025-02-01 04:35:23 +0800

    feat: add debounce to search bar

--- client/src/pages/dashboard/Dashboard.tsx
+++ client/src/pages/dashboard/Dashboard.tsx
@@ -1,55 +1,37 @@
-import { useState, useContext, useEffect } from 'react';
+import { useState, useContext, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { StockInfo } from '../../types/StockInfo';
 import DashboardItem from '../../components/dashboardItem/DashboardItem';
 import LoadingSpinner from '../../components/loadingSpinner/LoadingSpinner';
 import './Dashboard.css';
 import { AuthContext } from '../../context/AuthContext';
-
-const createStockObject = (stockData: any): StockInfo => ({
-  name: stockData['Company Name'] || 'Unknown Company',
-  symbol: stockData['Symbol'] || 'N/A',
-  price: parseFloat(stockData['Price']) || 0,
-  change: parseFloat(stockData['Change']?.replace(/[+,%]/g, '')) || 0,
-  percentChange: parseFloat(stockData['Change%']?.replace(/[+,%]/g, '')) || 0,
-});
+import { createStockObject } from './utils';
+import { fetchPortfolioData, fetchStockData } from './fetchData';
+import Dropdown from './Dropdown';
 
 function Dashboard() {
   const [searchTerm, setSearchTerm] = useState<string>('');
-  const [portfolioStockList, setPortfolioStockList] = useState<
-    { portfolioName: string; stocks: StockInfo[] }[]
-  >([]);
+  const [portfolioStockList, setPortfolioStockList] = useState<{ portfolioName: string; stocks: StockInfo[] }[]>([]);
   const [marketStockList, setMarketStockList] = useState<StockInfo[]>([]);
   const [portfolioLoading, setPortfolioLoading] = useState<boolean>(true);
   const [loading, setLoading] = useState<boolean>(true);
+  const [suggestions, setSuggestions] = useState<{ ticker: string; name: string }[]>([]);
+  const [showDropdown, setShowDropdown] = useState(false);
   const navigate = useNavigate();
-
   const { user, isAuthenticated } = useContext(AuthContext);
   const [showMessage, setShowMessage] = useState<boolean>(true);
   const [message, setMessage] = useState<string>('');
 
+  const debounceTimeout = useRef<NodeJS.Timeout | null>(null);
+
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
+
   const fetchPortfolio = async () => {
     const authToken = localStorage.getItem('authToken') || sessionStorage.getItem('authToken');
-    
     if (authToken) {
       setPortfolioLoading(true);
       try {
-        const payload = {
-          "method": "getPortfolio"
-        };
-        
-        const response = await fetch('https://dbvvd06r01.execute-api.ap-southeast-1.amazonaws.com/api/user/portfolio', {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${authToken}`,
-            },
-            body: JSON.stringify(payload)
-          });
-        
-        const result = await response.json();
-        const data = result.data;
-
+        const data = await fetchPortfolioData(authToken);
         const portfolios = Object.entries(data).map(([portfolioName, stocks]) => ({
           portfolioName,
           stocks: (stocks as any[]).map(createStockObject),
@@ -58,7 +40,7 @@ function Dashboard() {
         setPortfolioStockList(portfolios);
         setMessage('');
         setShowMessage(false);
-      } catch (error) { 
+      } catch (error) {
         setMessage('Error fetching portfolio: ' + error);
         setShowMessage(true);
       } finally {
@@ -70,38 +52,86 @@ function Dashboard() {
   const fetchStocks = async () => {
     setLoading(true);
     try {
-        const response = await fetch('https://dbvvd06r01.execute-api.ap-southeast-1.amazonaws.com/api/stock/get-most-active-stocks');
-        const data = await response.json();
-        const formattedData: StockInfo[] = JSON.parse(data).map(createStockObject);
-        setMarketStockList(formattedData);
+      const data = await fetchStockData();
+      const formattedData: StockInfo[] = data.map(createStockObject);
+      setMarketStockList(formattedData);
     } catch (error) {
       setMessage('Error fetching stocks: ' + error);
       setShowMessage(true);
     } finally {
-        setLoading(false);
+      setLoading(false);
+    }
+  };
+
+  const searchStocks = async (query: string) => {
+    if (query.length >= 1) {
+      try {
+        const response = await fetch(`http://localhost:8000/api/stocks/search/${query}`);
+        const result = await response.json();
+        setSuggestions(result);
+      } catch (error) {
+        console.error('Error fetching search suggestions:', error);
+      }
+    } else {
+      setSuggestions([]);
     }
   };
 
   useEffect(() => {
+    if (debounceTimeout.current) {
+      clearTimeout(debounceTimeout.current);
+    }
+
+    if (searchTerm.length >= 1) {
+      debounceTimeout.current = setTimeout(() => {
+        searchStocks(searchTerm);
+      }, 200);
+    } else {
+      setSuggestions([]);
+    }
+
+    return () => {
+      if (debounceTimeout.current) {
+        clearTimeout(debounceTimeout.current);
+      }
+    };
+  }, [searchTerm]);
+
+  useEffect(() => {
     fetchPortfolio();
     fetchStocks();
-}, []);
+  }, []);
+
+  useEffect(() => {
+    // Handle opening or closing dropdown based on suggestions
+    if (suggestions.length > 0) {
+      setShowDropdown(true);
+    } else {
+      setShowDropdown(false);
+    }
+  }, [suggestions]);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setShowDropdown(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, []);
 
   const userPortfolioStocks = portfolioStockList
-  .map((portfolio) => ({
-    portfolioName: portfolio.portfolioName,
-    stocks: portfolio.stocks.filter(
-      (stock) =>
-        stock.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        stock.symbol.toLowerCase().includes(searchTerm.toLowerCase())
-    ),
-  }))
-  .filter((portfolio) => portfolio.stocks.length > 0);
-
-  const filteredStocks = marketStockList.filter((stock: StockInfo) =>
-    stock.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-    stock.symbol.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+    .map((portfolio) => ({
+      portfolioName: portfolio.portfolioName,
+      stocks: portfolio.stocks.filter(
+        (stock) =>
+          stock.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          stock.symbol.toLowerCase().includes(searchTerm.toLowerCase())
+      ),
+    }))
+    .filter((portfolio) => portfolio.stocks.length > 0);
 
   const handleItemClick = (symbol: string) => {
     navigate(`/stock/${symbol}`);
@@ -109,39 +139,30 @@ function Dashboard() {
 
   return (
     <div className="dashboard">
+      {showDropdown && suggestions.length > 0 && (
+        <div className="overlay show" onClick={() => setShowDropdown(false)}></div>
+      )}
       <div className="dashboard__header">
         {isAuthenticated && user && (
           <div className="dashboard__user-portfolio__container">
-            <button type="submit" className="button">Create new portfolio</button>
+            <button type="submit" className="button">
+              Create new portfolio
+            </button>
             {portfolioLoading ? (
-                <LoadingSpinner />
-              ) : (
-                userPortfolioStocks.map((portfolio) => (
+              <LoadingSpinner />
+            ) : (
+              userPortfolioStocks.map((portfolio) => (
                 <div key={portfolio.portfolioName} className="portfolio-group">
                   <div className="portfolio__title">{portfolio.portfolioName}</div>
                   <div className="portfolio-stocks">
                     {portfolio.stocks.map((stock) => (
-                      <DashboardItem
-                        key={stock.symbol}
-                        {...stock}
-                        onClick={() => handleItemClick(stock.symbol)}
-                      />
+                      <DashboardItem key={stock.symbol} {...stock} onClick={() => handleItemClick(stock.symbol)} />
                     ))}
                   </div>
                 </div>
-                ))
-              )
-            }
-            {showMessage && (
-              <div className="message">
-                {message}
-              </div>
+              ))
             )}
-            {!showMessage && (
-              <div className="dashboard__user__portfolio">
-                
-              </div>
-                )}
+            {showMessage && <div className="message">{message}</div>}
           </div>
         )}
         <div className="dashboard__market__container">
@@ -154,18 +175,22 @@ function Dashboard() {
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
             />
+            {showDropdown && suggestions.length > 0 && (
+              <Dropdown
+                suggestions={suggestions}
+                dropdownRef={dropdownRef}
+                onItemClick={handleItemClick}
+                isOpen={showDropdown}
+              />
+            )}
           </div>
         </div>
       </div>
       {loading ? (
         <LoadingSpinner />
       ) : (
-        filteredStocks.map((stock: StockInfo) => (
-          <DashboardItem 
-            key={stock.symbol} 
-            {...stock}
-            onClick={() => handleItemClick(stock.symbol)}
-          />
+        marketStockList.map((stock: StockInfo) => (
+          <DashboardItem key={stock.symbol} {...stock} onClick={() => handleItemClick(stock.symbol)} />
         ))
       )}
     </div>
