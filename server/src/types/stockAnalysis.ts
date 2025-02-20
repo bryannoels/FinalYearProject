001afd48f13290b15c631ce219bea12ccf5574fc
bryannoels <bryannoelsalindeho23@gmail.com>
2025-02-20 17:10:15 +0800

    feat: create new type called StockRatings, and update StockAnalysis

--- server/src/types/stockAnalysis.ts
+++ server/src/types/stockAnalysis.ts
@@ -1,5 +1,13 @@
 export type StockAnalysis = {
+    verdict: string;
+    number_of_buy: number;
+    number_of_hold: number;
+    number_of_sell: number;
+    ratings: stockRatings[];
+};
+
+type stockRatings = {
     firm: string;
     analysis: string;
     rating: number;
-};
+}
