db3b3ee303dd161c17890cd4f1b22bbb9b78fac6
albione <50472797+albione@users.noreply.github.com>
2025-02-23 16:10:45 +0800

    feat: most active stock avoid getting blocked

--- server/src/stocks/getTopStock.py
+++ server/src/stocks/getTopStock.py
@@ -5,7 +5,10 @@ import re
 
 def get_top_10_most_active_stocks():
     url = 'https://finance.yahoo.com/markets/stocks/most-active/'
-    response = requests.get(url)
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    }
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
         stocks_data = []
