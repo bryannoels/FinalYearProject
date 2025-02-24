35c28b4b7b56b016b0a2ff83fdf18a83b5d3278b
bryannoels <bryannoelsalindeho23@gmail.com>
2025-02-24 17:50:32 +0800

    feat: add nulable for stock type

--- client/src/types/Stock.tsx
+++ client/src/types/Stock.tsx
@@ -9,15 +9,15 @@ import { PeRatio } from './PeRatio';
 import { Dividend } from './Dividend';
 
 export type Stock = {
-    info: StockInfo;
-    profile: StockProfile;
-    detail: StockDetail;
-    price: StockPrice[];
-    forecast: Forecast;
-    analysis: StockAnalysis;
-    eps: Eps[];
-    peRatio: PeRatio[];
-    dividends: Dividend[];
-    growthRate: string;
-    bondYield: string;
+    info: StockInfo  | null;
+    profile: StockProfile  | null;
+    detail: StockDetail | null;
+    price: StockPrice[] | null;
+    forecast: Forecast | null;
+    analysis: StockAnalysis | null;
+    eps: Eps[] | null;
+    peRatio: PeRatio[] | null;
+    dividends: Dividend[] | null;
+    growthRate: string | null;
+    bondYield: string | null;
 }
