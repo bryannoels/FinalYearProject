de77df3940ca4bef6c762bb25f231732b9518436
bryannoels <bryannoelsalindeho23@gmail.com>
2025-03-03 20:57:42 +0800

    feat: move fetch Data

---
+++ client/src/pages/utils/fetchData.ts
@@ -0,0 +1,14 @@
+export const fetchPortfolioData = async (authToken: string) => {
+    const payload = { method: 'getPortfolio' };
+    const response = await fetch('https://dbvvd06r01.execute-api.ap-southeast-1.amazonaws.com/api/user/portfolio', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${authToken}`,
+      },
+      body: JSON.stringify(payload),
+    });
+  
+    const result = await response.json();
+    return result.data;
+  };
