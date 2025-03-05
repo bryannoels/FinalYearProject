fbc38db04660d510aa17ce9114490e9a873522b8
bryannoels <bryannoelsalindeho23@gmail.com>
2025-03-06 03:27:29 +0800

    feat: separate title and search bar from the list

--- client/src/pages/currentMarket/currentMarket.tsx
+++ client/src/pages/currentMarket/currentMarket.tsx
@@ -1,23 +1,16 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { fetchData, getCachedData, setCachedData } from '../../components/utils/utils';
 import { StockInfo } from '../../types/StockInfo';
 import DashboardItem from '../../components/dashboardItem/DashboardItem';
 import LoadingSpinner from '../../components/loadingSpinner/LoadingSpinner';
 import { createStockObject } from '../utils/utils';
-import Dropdown from '../../components/dropdown/Dropdown';
-import { searchStocks } from '../utils/searchStocks';
 
 export function CurrentMarket () {
-    const [searchTerm, setSearchTerm] = useState<string>('');
     const [marketStockList, setMarketStockList] = useState<StockInfo[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
-    const debounceTimeout = useRef<NodeJS.Timeout | null>(null);
     const [showMessage, setShowMessage] = useState<boolean>(true);
     const [message, setMessage] = useState<string>('');
-    const [searchStockResult, setSuggestions] = useState<{ ticker: string; name: string }[]>([]);
-    const [showSearchedStocks, setShowDropdown] = useState(false);
-    const dropdownRef = useRef<HTMLDivElement | null>(null);
     const navigate = useNavigate();
     
 
@@ -47,47 +40,11 @@ export function CurrentMarket () {
       };
 
       useEffect(() => {
-        debounceTimeout.current && clearTimeout(debounceTimeout.current);
-    
-        if (searchTerm.length >= 1) {
-          debounceTimeout.current = setTimeout(() => searchStocks(searchTerm, setSuggestions), 200);
-        } else {
-          setSuggestions([]);
-        }
-    
-        return () => { debounceTimeout.current && clearTimeout(debounceTimeout.current); };
-      }, [searchTerm]);
-
-      useEffect(() => {
         fetchStocks();
-      }, []);
-
-      useEffect(() => {
-        setShowDropdown(searchStockResult.length > 0);
-      }, [searchStockResult]);      
+      }, []);   
 
     return (
         <div>
-            <div className="dashboard__market__container">
-                <p className="market__title">Current Market</p>
-                <div className="market__search-box">
-                <input
-                    type="text"
-                    className="dashboard__search"
-                    placeholder="Search stocks..."
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                />
-                {showSearchedStocks && searchStockResult.length > 0 && (
-                    <Dropdown
-                    suggestions={searchStockResult}
-                    dropdownRef={dropdownRef}
-                    onItemClick={handleItemClick}
-                    isOpen={showSearchedStocks}
-                    />
-                )}
-                </div>
-            </div>
             {loading ? (
                 <LoadingSpinner />
             ) : (
--- client/src/pages/stockList/StockList.tsx
+++ client/src/pages/stockList/StockList.tsx
@@ -1,9 +1,60 @@
+import { useState, useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { CurrentMarket } from '../currentMarket/currentMarket';
+import Dropdown from '../../components/dropdown/Dropdown';
+import { searchStocks } from '../utils/searchStocks';
 import './StockList.css';
 
 function StockList() {
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const debounceTimeout = useRef<NodeJS.Timeout | null>(null);
+  const [searchStockResult, setSuggestions] = useState<{ ticker: string; name: string }[]>([]);
+  const [showSearchedStocks, setShowDropdown] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement | null>(null);
+  const navigate = useNavigate();
+
+  const handleItemClick = (symbol: string) => {
+    navigate(`/stock/${symbol}`);
+  };
+
+  useEffect(() => {
+    debounceTimeout.current && clearTimeout(debounceTimeout.current);
+
+    if (searchTerm.length >= 1) {
+      debounceTimeout.current = setTimeout(() => searchStocks(searchTerm, setSuggestions), 200);
+    } else {
+      setSuggestions([]);
+    }
+
+    return () => { debounceTimeout.current && clearTimeout(debounceTimeout.current); };
+  }, [searchTerm]);
+
+  useEffect(() => {
+    setShowDropdown(searchStockResult.length > 0);
+  }, [searchStockResult]);   
+
   return (
     <div className="stock-list">
+      <div className="dashboard__market__container">
+          <p className="market__title">Current Market</p>
+          <div className="market__search-box">
+          <input
+              type="text"
+              className="dashboard__search"
+              placeholder="Search stocks..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          {showSearchedStocks && searchStockResult.length > 0 && (
+              <Dropdown
+              suggestions={searchStockResult}
+              dropdownRef={dropdownRef}
+              onItemClick={handleItemClick}
+              isOpen={showSearchedStocks}
+              />
+          )}
+          </div>
+      </div>
       <CurrentMarket />
     </div>
   );
