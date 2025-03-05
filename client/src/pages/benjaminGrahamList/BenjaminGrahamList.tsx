8c268ed6f1dfba52652a725bb49d7aa60a1f5842
bryannoels <bryannoelsalindeho23@gmail.com>
2025-03-06 03:33:47 +0800

    feat: adjust benjamin graham list

--- client/src/pages/benjaminGrahamList/BenjaminGrahamList.tsx
+++ client/src/pages/benjaminGrahamList/BenjaminGrahamList.tsx
@@ -2,9 +2,30 @@ import { CurrentMarket } from '../currentMarket/currentMarket';
 import './BenjaminGrahamList.css';
 
 function BenjaminGrahamList() {
+
   return (
     <div className="benjamin-graham-list">
-      <CurrentMarket />
+      {/* <div className="dashboard__market__container">
+          <p className="market__title">Current Market</p>
+          <div className="market__search-box">
+          <input
+              type="text"
+              className="dashboard__search"
+              placeholder="Search stocks..."
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          {showSearchedStocks && searchStockResult.length > 0 && (
+              <Dropdown
+              suggestions={searchStockResult}
+              dropdownRef={dropdownRef}
+              onItemClick={handleItemClick}
+              isOpen={showSearchedStocks}
+              />
+          )}
+          </div>
+      </div> */}
+      {/* <CurrentMarket /> */}
     </div>
   );
 }
