7b0679e100c89e94660a215605b7c85f82ad8645
bryannoels <bryannoelsalindeho23@gmail.com>
2025-03-06 03:19:44 +0800

    feat: create Benjamin Graham List

---
+++ client/src/pages/benjaminGrahamList/BenjaminGrahamList.tsx
@@ -0,0 +1,12 @@
+import { CurrentMarket } from '../currentMarket/currentMarket';
+import './BenjaminGrahamList.css';
+
+function BenjaminGrahamList() {
+  return (
+    <div className="benjamin-graham-list">
+      <CurrentMarket />
+    </div>
+  );
+}
+
+export default BenjaminGrahamList;
