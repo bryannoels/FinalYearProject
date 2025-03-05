21a25345c2ba5714d917a792ba5099d963b876b4
bryannoels <bryannoelsalindeho23@gmail.com>
2025-03-06 03:26:42 +0800

    feat: add benjamin graham list to the routes

--- client/src/navigation/router.tsx
+++ client/src/navigation/router.tsx
@@ -10,6 +10,7 @@ import Login from "../pages/login/Login.tsx"
 import Signup from "../pages/signup/Signup.tsx"
 import StockDetails from "../pages/stockDetails/StockDetails.tsx";
 import StockList from "../pages/stockList/StockList.tsx";
+import BenjaminGrahamList from "../pages/benjaminGrahamList/BenjaminGrahamList.tsx";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -18,6 +19,7 @@ export const router = createBrowserRouter(
       <Route path="/" element={<Layout />}>
         <Route index element={<Dashboard />} />
         <Route path="stocklist" element={<StockList />} />
+        <Route path="benjamingrahamlist" element={<BenjaminGrahamList />} />
         <Route path="stock/:symbol" element={<StockDetails />} />
       </Route>
 
