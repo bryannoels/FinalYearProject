be5ad3883dfa954a5a6965931997dde9bf14fb5e
bryannoels <bryannoelsalindeho23@gmail.com>
2025-03-06 18:34:29 +0800

    feat: add date time to the get and set cache

--- client/src/components/utils/utils.tsx
+++ client/src/components/utils/utils.tsx
@@ -1,16 +1,24 @@
-export const getCachedData = (key: string): any => {
+import { CachedData } from "../../types/CachedData";
+
+export const getCachedData = (key: string): CachedData | null => {
     const cachedItem = localStorage.getItem(key);
     if (!cachedItem) return null;
+
     const { data, timestamp } = JSON.parse(cachedItem);
-    if (Date.now() - timestamp > 3600000) {
+
+    if (Date.now() - new Date(timestamp).getTime() > 3600000) {
         localStorage.removeItem(key);
         return null;
     }
-    return data;
+
+    return {
+        data,
+        timestamp,
+    };
 };
 
 export const setCachedData = (key: string, data: any) => {
-    localStorage.setItem(key, JSON.stringify({ data, timestamp: Date.now() }));
+    localStorage.setItem(key, JSON.stringify({ data: data.data, timestamp: data.timestamp }));
 };
 
 export const fetchData = async (url: string) => {
