426e5c5d67a330cda44a8b7b98b76fbef97be3bf
bryannoels <bryannoelsalindeho23@gmail.com>
2025-03-06 13:56:16 +0800

    feat: move current market out of dashboard

--- client/src/pages/dashboard/Dashboard.tsx
+++ client/src/pages/dashboard/Dashboard.tsx
@@ -1,12 +1,10 @@
 import { Portfolio } from '../portfolio/portfolio';
-import { CurrentMarket } from '../currentMarket/currentMarket';
 import './Dashboard.css';
 
 function Dashboard() {
   return (
     <div className="dashboard">
       <Portfolio />
-      <CurrentMarket />
     </div>
   );
 }
