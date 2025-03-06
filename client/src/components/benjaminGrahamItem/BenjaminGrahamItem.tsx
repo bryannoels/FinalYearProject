54525aedd1e25bdcdeaddf4ebfc9a7e26eaba48f
bryannoels <bryannoelsalindeho23@gmail.com>
2025-03-07 03:31:43 +0800

    feat: style the benjaminGrahamItem component (list)

--- client/src/components/benjaminGrahamItem/BenjaminGrahamItem.tsx
+++ client/src/components/benjaminGrahamItem/BenjaminGrahamItem.tsx
@@ -4,6 +4,7 @@ import './BenjaminGrahamItem.css';
 
 interface BenjaminGrahamItemProps extends BenjaminGrahamStockInfo {
     onClick: () => void;
+    sortBy: string;
 }
 
 const BenjaminGrahamItem: React.FC<BenjaminGrahamItemProps> = ({ symbol, companyName, defensiveValue, defensive, enterprisingValue, enterprising, overallValue, sortBy, onClick = () => {}}) => {
@@ -14,11 +15,18 @@ const BenjaminGrahamItem: React.FC<BenjaminGrahamItemProps> = ({ symbol, company
                 <p className="benjamin__graham__item__symbol">{symbol}</p>
             </div>
             <div className="benjamin__graham__item__right" onClick={onClick}>
-                <p className="benjamin__graham__item__price">Sort by {sortBy}</p>
-                <p className={`benjamin__graham__item__change`}>
-                Defensive Value: {defensiveValue}
-                    Enterprising Value: {enterprisingValue}
-                    Overall Value: {overallValue}
+                <p className="benjamin__graham__item__sort__by">
+                    Sort by {sortBy == 'defensive' ? 
+                    'Long Term Value' : sortBy == 'enterprising' ?
+                    'Short Term Value' : 'Overall Value'
+                    }
+            </p>
+                <p className={`benjamin__graham__item__value`}>
+                {
+                    sortBy === 'defensive' ? `${defensiveValue} out of 7 points`
+                    : sortBy === 'enterprising' ? `${enterprisingValue} out of 7 points`
+                    : `${overallValue} out of 14 points`
+                }
                 </p>
             </div>
         </div>
