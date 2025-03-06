094b6501435a1eca8925874d7888031151a60ebc
bryannoels <bryannoelsalindeho23@gmail.com>
2025-03-06 13:58:46 +0800

    feat: benjamin graham text

--- client/src/pages/benjaminGrahamList/BenjaminGrahamList.tsx
+++ client/src/pages/benjaminGrahamList/BenjaminGrahamList.tsx
@@ -5,26 +5,11 @@ function BenjaminGrahamList() {
 
   return (
     <div className="benjamin-graham-list">
-      {/* <div className="dashboard__market__container">
-          <p className="market__title">Current Market</p>
+      <div className="dashboard__market__container">
+          <p className="market__title">Benjamin Graham</p>
           <div className="market__search-box">
-          <input
-              type="text"
-              className="dashboard__search"
-              placeholder="Search stocks..."
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          {showSearchedStocks && searchStockResult.length > 0 && (
-              <Dropdown
-              suggestions={searchStockResult}
-              dropdownRef={dropdownRef}
-              onItemClick={handleItemClick}
-              isOpen={showSearchedStocks}
-              />
-          )}
           </div>
-      </div> */}
+      </div>
       {/* <CurrentMarket /> */}
     </div>
   );
