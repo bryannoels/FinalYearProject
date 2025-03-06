7088e21ac68bde7de6990df03517a731d444161e
bryannoels <bryannoelsalindeho23@gmail.com>
2025-03-07 03:18:49 +0800

    feat: create benjamin graham stock info

---
+++ client/src/types/BenjaminGrahamStockInfo.tsx
@@ -0,0 +1,10 @@
+export type BenjaminGrahamStockInfo = {
+    symbol: string;
+    companyName: string;
+    defensiveValue: number;
+    defensive: string;
+    enterprisingValue: number;
+    enterprising: string;
+    overallValue: number;
+};
+  
