6629a53c90fdc9efc97437badc7d9464fe0fc0b4
bryannoels <bryannoelsalindeho23@gmail.com>
2025-03-12 02:07:21 +0800

    feat: create new type "BenjaminGrahamStockPagination"

---
+++ client/src/types/BenjaminGrahamStockPagination.tsx
@@ -0,0 +1,8 @@
+export type BenjaminGrahamStockPagination = {
+    currentPage: 1,
+    totalPages: 51,
+    pageSize: 10,
+    totalItems: 502,
+    hasNextPage: true,
+    hasPreviousPage: false
+}
