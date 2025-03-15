868ed2a8e65258ead3e72caf70f3d4f5d20990a0
bryannoels <bryannoelsalindeho23@gmail.com>
2025-03-15 12:24:35 +0800

    feat: update current yield to scrap directly from API

--- server/src/stocks/getBenjaminGrahamValue.py
+++ server/src/stocks/getBenjaminGrahamValue.py
@@ -1,12 +1,13 @@
 import yfinance as yf
 import sys
 import json
+from getAaaCorporateBondYield import get_aaa_corporate_bond_yield
 
 def get_benjamin_graham_value(stock_symbol):
     stock = yf.Ticker(stock_symbol)
     eps = stock.info.get("trailingEps", None)
     growth = stock.growth_estimates.get("stockTrend", {}).get("+1y", 0)*100
-    current_yield = 5.32
+    current_yield = float(get_aaa_corporate_bond_yield()['aaaCorporateBondYield'][:-1])
     intrinsic_value = round(eps * (8.5 + 2 * growth) * 4.4 / current_yield, 2)
     print("EPS:", eps)
     print("Growth:", growth)
