787e128d7694eb9858dfcd95ef76dca3b661948c
bryannoels <bryannoelsalindeho23@gmail.com>
2025-04-09 23:39:09 +0800

    refactor: refactor stock route

--- server/src/routes/stockRoute.ts
+++ server/src/routes/stockRoute.ts
@@ -1,36 +1,21 @@
 import express from 'express';
-import { 
-  getStockData, 
-  getStockProfile,
-  getTopStocks, 
-  getAnalysis, 
-  getHistoricalData, 
-  getForecastData, 
-  getEPSData, 
-  getPeRatioData,
-  getAaaCorporateBondYield,
-  searchStock,
-  getBenjaminGrahamList,
-  getDCFValue,
-  getDDMValue,
-  getBenjaminGrahamValue
-} from '../controllers/stockController';
+import stockControllers from '../controllers/stockController';
 
 const router = express.Router();
 
-router.get('/info/:symbol', getStockData);
-router.get('/get-profile/:symbol', getStockProfile);
-router.get('/get-top-stocks', getTopStocks);
-router.get('/analysis/:symbol', getAnalysis);
-router.get('/get-historical-data/:symbol', getHistoricalData);
-router.get('/get-forecast/:symbol', getForecastData);
-router.get('/get-eps/:symbol', getEPSData);
-router.get('/get-pe-ratio/:symbol', getPeRatioData);
-router.get('/get-aaa-corp-bond-yield', getAaaCorporateBondYield);
-router.get('/search/:query', searchStock);
-router.get('/get-benjamin-graham-list', getBenjaminGrahamList);
-router.get('/get-dcf-value/:symbol', getDCFValue);
-router.get('/get-ddm-value/:symbol', getDDMValue);
-router.get('/get-benjamin-graham-value/:symbol', getBenjaminGrahamValue);
+router.get('/info/:symbol', stockControllers.getStockData);
+router.get('/get-profile/:symbol', stockControllers.getStockProfile);
+router.get('/get-top-stocks', stockControllers.getTopStocks);
+router.get('/analysis/:symbol', stockControllers.getAnalysis);
+router.get('/get-historical-data/:symbol', stockControllers.getHistoricalData);
+router.get('/get-forecast/:symbol', stockControllers.getForecastData);
+router.get('/get-eps/:symbol', stockControllers.getEPSData);
+router.get('/get-pe-ratio/:symbol', stockControllers.getPeRatioData);
+router.get('/get-aaa-corp-bond-yield', stockControllers.getAaaCorporateBondYield);
+router.get('/search/:query', stockControllers.searchStock);
+router.get('/get-benjamin-graham-list', stockControllers.getBenjaminGrahamList);
+router.get('/get-dcf-value/:symbol', stockControllers.getDCFValue);
+router.get('/get-ddm-value/:symbol', stockControllers.getDDMValue);
+router.get('/get-benjamin-graham-value/:symbol', stockControllers.getBenjaminGrahamValue);
 
 export default router;
