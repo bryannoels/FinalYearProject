a7080dab0f2fa71c9fc0f9f41e69a7ee632cc0e4
bryannoels <bryannoelsalindeho23@gmail.com>
2025-04-17 01:37:12 +0800

    feat: add router path

--- client/src/navigation/router.tsx
+++ client/src/navigation/router.tsx
@@ -11,6 +11,7 @@ import Signup from "../pages/signup/Signup";
 import StockDetails from "../pages/stockDetails/StockDetails";
 import StockList from "../pages/stockList/StockList";
 import BenjaminGrahamList from "../pages/benjaminGrahamList/BenjaminGrahamList";
+import IntrinsicValueList from "../pages/intrinsicValueList/IntrinsicValueList";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
@@ -21,6 +22,7 @@ export const router = createBrowserRouter(
         <Route path="stocklist" element={<StockList />} />
         <Route path="benjamingrahamlist" element={<BenjaminGrahamList />} />
         <Route path="stock/:symbol" element={<StockDetails />} />
+        <Route path="intrinsicvaluelist" element={<IntrinsicValueList />} />
       </Route>
 
       {/* Unique route without Layout */}
